dbfa4d9ff191ca6209ad4ea1ae8903bb0f9d2073
freakpari <parivashcheraghi40@gmail.com>
2026-06-07 11:19:28 +0330

    styles for landing activity added| parivash cheraghi

--- app/src/main/java/com/example/pixo/LandingActivity.kt
+++ app/src/main/java/com/example/pixo/LandingActivity.kt
@@ -22,6 +22,10 @@ import androidx.compose.ui.text.font.FontFamily
 
 
 val forteFont = FontFamily(Font(R.font.forte))
+val interFont = FontFamily(
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold)
+)
 
 class LandingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,46 +55,47 @@ fun LandingScreen() {
                 .padding(horizontal = 40.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(100.dp))
+            Spacer(modifier = Modifier.height(116.dp))
             Image(
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = "App Logo",
-                modifier = Modifier.size(130.dp)
+                modifier = Modifier.size(120.dp)
             )
 
             Text(
                 text = "Pixo",
                 color = White,
-                fontSize = 50.sp,
+                fontSize = 64.sp,
                 fontWeight = FontWeight.Bold,
                 fontFamily = forteFont
 
             )
 
-            Spacer(modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.height(144.dp))
 
             Text(
                 text = "Welcome!",
                 color = White,
                 fontSize = 32.sp,
-                fontWeight = FontWeight.Bold
-            )
+                fontFamily = interFont,
+                fontWeight = FontWeight.Bold            )
 
             Spacer(modifier = Modifier.height(40.dp))
 
             Button(
                 onClick = { /*  login code later  */ },
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp),
+                    .width(350.dp)
+                    .height(42.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Primary1),
-                shape = RoundedCornerShape(12.dp)
+                shape = RoundedCornerShape(8.dp)
             ) {
                 Text(
                     text = "Login",
                     color = Primary6,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
+                    fontSize = 14.sp,
+                    fontFamily = interFont,
+                    fontWeight = FontWeight.SemiBold
                 )
             }
 
@@ -99,16 +104,17 @@ fun LandingScreen() {
             Button(
                 onClick = { /* sign up code later*/ },
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp),
+                    .width(350.dp)
+                    .height(42.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Primary1),
-                shape = RoundedCornerShape(12.dp)
+                shape = RoundedCornerShape(8.dp)
             ) {
                 Text(
                     text = "Sign Up",
                     color = Primary6,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
+                    fontSize = 14.sp,
+                    fontFamily = interFont,
+                    fontWeight = FontWeight.SemiBold
                 )
             }
 
