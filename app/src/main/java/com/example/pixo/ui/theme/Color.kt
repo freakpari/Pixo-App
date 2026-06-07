d42dd22d174c23db253cf09e9a9728e241960af3
freakpari <parivashcheraghi40@gmail.com>
2026-06-07 10:43:03 +0330

    Theme ui files added | parivash cheraghi

---
+++ app/src/main/java/com/example/pixo/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.pixo.ui.theme // نام پکیج خودت رو چک کن
+
+import androidx.compose.ui.graphics.Color
+
+val Primary1 = Color(0xFFEEF6FB)
+val Primary6 = Color(0xFF0083DA)
+val Primary7 = Color(0xFF0069AE)
+val Primary8 = Color(0xFF004F83)
+val Secondary4 = Color(0xFFA659DE)
+val White = Color(0xFFFFFFFF)
---
+++ app/src/main/java/com/example/pixo/ui/theme/Theme.kt
@@ -0,0 +1,41 @@
+package com.example.pixo.ui.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+
+import androidx.core.view.WindowCompat
+
+private val LightColorScheme = lightColorScheme(
+    primary = Primary7,
+    secondary = Secondary4,
+    tertiary = Primary6,
+    background = White
+)
+
+@Composable
+fun PixoTheme(
+    content: @Composable () -> Unit
+) {
+    val colorScheme = LightColorScheme
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Primary8.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = com.example.pixo.ui.theme.Typography,
+        content = content
+    )
+}
