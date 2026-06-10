bb06e2d8051f80b7caf20b2edfb2a8324a6d22ac
freakpari <parivashcheraghi40@gmail.com>
2026-06-10 19:51:03 +0330

    colors updated| parivash cheraghi

--- app/src/main/java/com/example/pixo/ui/theme/Color.kt
+++ app/src/main/java/com/example/pixo/ui/theme/Color.kt
@@ -3,8 +3,16 @@ package com.example.pixo.ui.theme // نام پکیج خودت رو چک کن
 import androidx.compose.ui.graphics.Color
 
 val Primary1 = Color(0xFFEEF6FB)
+
+val Primary2 =Color(0xFFCCE6F8)
+
+val Primary5 =Color(0xFF40A2E3)
 val Primary6 = Color(0xFF0083DA)
 val Primary7 = Color(0xFF0069AE)
 val Primary8 = Color(0xFF004F83)
+
+val Primary9 = Color(0xFF003B62)
+
+val Primary10 = Color(0xFF002842)
 val Secondary4 = Color(0xFFA659DE)
 val White = Color(0xFFFFFFFF)
