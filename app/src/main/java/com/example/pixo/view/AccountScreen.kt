17b6df20a593a53c81eec607610231a1421bc7c2
freakpari <parivashcheraghi40@gmail.com>
2026-06-17 12:14:21 +0330

    add 2 fake layout for home and account page| parivash cheraghi

---
+++ app/src/main/java/com/example/pixo/view/AccountScreen.kt
@@ -0,0 +1,24 @@
+package com.example.pixo.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.example.pixo.ui.theme.Primary8
+
+@Composable
+fun AccountScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Account Screen (Fake Layout)", color = Primary8, fontSize = 20.sp, fontFamily = interFont)
+    }
+}
